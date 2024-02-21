06c6ad8f4da21f010b15be5a0da7ba5f92a18339
admin <shivani91jm@gmail.com>
2024-02-21 07:09:19 -0800

    this is first commit

--- lib/Utils/AppFontFamily.dart
+++ lib/Utils/AppFontFamily.dart
@@ -1,3 +1,18 @@
-class AppFontFamilyClass{
+import 'package:flutter/material.dart';
 
+class AppFontFamilyClass{
+  static const String   fontfamily= "Lexend";
+  static const TextStyle regular = TextStyle(
+    fontFamily: fontfamily,
+    fontWeight: FontWeight.normal,
+    fontSize: 16.0,
+    // Add other properties as needed
+  );
+
+  static const TextStyle bold = TextStyle(
+    fontFamily: fontfamily,
+    fontWeight: FontWeight.bold,
+    fontSize: 16.0,
+    // Add other properties as needed
+  );
 }
